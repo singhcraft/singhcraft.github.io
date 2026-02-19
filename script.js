e1a33e291e5d84d5682c7f397b82f3f0b47381f6
singhcraft <amarjeetsingh.edu1@gmail.com>
2026-02-19 22:29:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+const cards=document.querySelectorAll('.card');
+const girls=document.querySelectorAll('.girl');
+
+window.addEventListener('scroll',()=>{
+
+cards.forEach(card=>{
+if(card.getBoundingClientRect().top < window.innerHeight-100){
+card.classList.add('show');
+}
+});
+
+girls.forEach(girl=>{
+if(window.scrollY>50){
+girl.classList.add("show");
+}
+});
+
+});
 
