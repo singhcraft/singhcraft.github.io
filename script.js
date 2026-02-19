0abc008688b380d9cedf33626a8861f15d8debc7
singhcraft <amarjeetsingh.edu1@gmail.com>
2026-02-19 21:31:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+const faders = document.querySelectorAll(".fade");
+
+const appear = new IntersectionObserver(entries=>{
+entries.forEach(entry=>{
+if(entry.isIntersecting){
+entry.target.classList.add("show");
+}
+});
+});
+
+faders.forEach(el=>{
+appear.observe(el);
+});
 
